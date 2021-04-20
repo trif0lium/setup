155439a32ea9f29bcc5b23a6243d5610c6bcf419
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-20 17:33:30 +0700

    feat(install.sh): update

--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 cd $(dirname $0)
 
+sudo -v
+caffeinate &
+
+# Install Xcode Command Line Tools.
+if ! xcode-select --print-path &> /dev/null; then
+  xcode-select --install &> /dev/null
+  until xcode-select --print-path &> /dev/null; do
+    sleep 5
+  done
+fi;
+
 # Install Homebrew.
 yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Oh My Zsh.
+yes | sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p ~/Workspace/fs
+
+git clone https://github.com/trif0lium/setup.git ~/Workspace/.dot
+
+cd ~/Workspace/.dot && sh ./init.sh
+
+killall caffeinate
