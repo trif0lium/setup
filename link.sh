0f8b18678eb6fef1288c723e365f7d57c17521fc
FleeingSunlight <gh@fleeingsunlight.dev>
2021-04-22 11:37:24 +0700

    feat(.tool-versions): init

--- link.sh
+++ link.sh
@@ -1,9 +1,14 @@
 cd $(dirname $0)
+
 ln -s $(pwd)/.gitconfig ~/.gitconfig
 ln -s $(pwd)/config/git ~/.config/git
+
 ln -s $(pwd)/config/bat ~/.config/bat
 ln -s $(pwd)/config/nvim ~/.config/nvim
 
 # .zshrc
 mv ~/.zshrc ~/.zshrc.copy
 ln -s $(pwd)/.zshrc ~/.zshrc
+
+# asdf
+ln -s $(pwd)/.tool-versions ~/.tool-versions
