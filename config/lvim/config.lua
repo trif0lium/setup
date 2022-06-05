e7d0ff39222a5024fd15ab562e147db3e7c389b1
trif0lium <gh@fleeingsunlight.dev>
2022-06-05 11:49:52 +0700

    feat(lvim): lvim.builtin

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,8 +1,14 @@
 lvim.format_on_save = true
 lvim.colorscheme = "moonfly"
 lvim.leader = "space"
+lvim.builtin.alpha.active = true
 lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = true
 lvim.builtin.gitsigns.active = false
+lvim.builtin.treesitter.ensure_installed = "maintained"
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.lsp.diagnostics.virtual_text = false
+
 lvim.plugins = {
   {"lunarvim/colorschemes"},
   {"Yggdroot/indentLine"},
