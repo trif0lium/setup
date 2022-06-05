0de2ba5718f89859bd9c87c3086778734d20bdaa
trif0lium <gh@fleeingsunlight.dev>
2022-06-05 11:33:00 +0700

    feat(lvim): lvim.plugins

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -1,4 +1,11 @@
 lvim.format_on_save = true
-lvim.colorscheme = "onedarker"
+lvim.colorscheme = "moonfly"
 lvim.leader = "space"
-lvim.builtin.notify.active = false
+lvim.builtin.notify.active = true
+lvim.builtin.gitsigns.active = false
+lvim.plugins = {
+  {"lunarvim/colorschemes"},
+  {"Yggdroot/indentLine"},
+  {"ntpeters/vim-better-whitespace"},
+  {"bluz71/vim-moonfly-colors"},
+}
